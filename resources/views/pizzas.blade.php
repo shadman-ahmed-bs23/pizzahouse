2242cc4935caf23c268b537bae8b2351499ba1a2
Shadman Ahmed <shadmanahmed874@gmail.com>
2021-04-01 10:47:50 +0600

    Add site layout

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
+@section('content')
     <h2>Pizzas</h2>
     <!-- For Loop -->
     @for ($i = 0; $i < 5; $i++)
@@ -21,7 +13,4 @@
             <span> - first in the loop</span>
         @endif
     @endforeach
-
-</body>
-
-</html>
+@endsection

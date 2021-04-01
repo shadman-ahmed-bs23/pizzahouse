793511cf2f8e173ab4af137f48efe85546b4d431
Shadman Ahmed <shadmanahmed874@gmail.com>
2021-04-01 10:59:29 +0600

    Adding css and images

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -2,15 +2,12 @@
 
 @section('content')
     <h2>Pizzas</h2>
-    <!-- For Loop -->
-    @for ($i = 0; $i < 5; $i++)
-        <p>The Value of i is {{ $i }} </p>
-    @endfor
-
     @foreach ($pizzas as $pizza)
         <div> {{ $loop->index }} {{ $pizza['type'] }} - {{ $pizza['base'] }} - {{ $pizza['price'] }}</div>
         @if ($loop->first)
             <span> - first in the loop</span>
         @endif
     @endforeach
+
+    <img src="/images/pizza-house.jpg" alt="">
 @endsection
